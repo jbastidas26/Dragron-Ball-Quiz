1cf0e05c5de1d3ae5f5408c57a7c9097471b1223
jbastidas26 <43036752+jbastidas26@users.noreply.github.com>
2019-04-22 17:03:07 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,30 @@
+document.getElementById('btn').addEventListener('click', function() {
+  var formInput = document.getElementById("numberForm");
+  //Code here for step 4
+  var gender=formInput.gender.value;
+  alert(gender);
+});
+document.getElementById('jbt').addEventListener('click', function() {
+  var formInput = document.getElementById("question2");
+  //Code here for step 4
+  var gender=formInput.gender.value;
+  alert(gender);
+});
+document.getElementById('rtb').addEventListener('click', function() {
+  var formInput = document.getElementById("question3");
+  //Code here for step 4
+  var gender=formInput.gender.value;
+  alert(gender);
+});
+document.getElementById('bbt').addEventListener('click', function() {
+  var formInput = document.getElementById("question4");
+  //Code here for step 4
+  var gender=formInput.gender.value;
+  alert(gender);
+});
+
+
+
+
+
+
